a1a508da988ff98e1a194a7c96aef9688555c7e5
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-09-09 16:16:43 -0500

    Hot fix

--- public/js/app.js
+++ public/js/app.js
@@ -56776,7 +56776,7 @@ window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]);
-Vue.component('tableemployes-component', __webpack_require__(/*! ./components/TableEmployeesComponent.vue */ "./resources/js/components/TableEmployeesComponent.vue")["default"]);
+Vue.component('tableemployees-component', __webpack_require__(/*! ./components/TableEmployeesComponent.vue */ "./resources/js/components/TableEmployeesComponent.vue")["default"]);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
