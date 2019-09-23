727a13f2114bef95d36fa80ec373f188eb8c0b26
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-09-23 12:01:53 -0500

    Se reparro la tabla

--- resources/js/components/TableEmployeesComponent.vue
+++ resources/js/components/TableEmployeesComponent.vue
@@ -3,31 +3,18 @@
     <thead>
         <tr class="thead-dark">
             <th>N. CONTROL</th>
+            <th>CORREO</th>
             <th>NOMBRE</th>
-            <th>ACTUALIZADO</th>
-            <th>CREADO</th>
-            <th class="pt-1 pb-1"><button class="btn btn-primary col-12" type="button" data-toggle="modal" data-target="#addTrabajador">Agregar</button></th>
+            <th>DEPENDENCIA</th>
         </tr>
     </thead>
 
     <tbody>
-        <tr v-for="employee in employees" v-bind:key="employee.email">
+        <tr v-for="employee in employees" v-bind:key="">
             <td>{{ employee.numconemp }}</td>
-            <td>{{ employee.nombre + ' ' + employee.apepat }}</td>
+            <td>{{ employee.email }}</td>
+            <td>{{ employee.nombre + ' ' + employee.apepat + ' ' + employee.apemat }}</td>
             <td>{{ employee.dependencia }}</td>
-
-            <td class="pt-1 pb-1">
-                <div class="dropdown" :data-item="employee.numconemp">
-                    <button :id="'menuRow' + employee.nombre" class="btn btn-secondary dropdown-toggle w-100" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        settings
-                    </button>
-                    
-                    <div class="dropdown-menu shadow trabajador-actions" :aria-labelledby="'menuRow' + employee.nombre">
-                        <a class="dropdown-item btn-warning" type="button" data-toggle="modal" data-target="#editTrabajador">Editar</a>
-                        <a class="dropdown-item btn-danger" :href="'{$base_url}trabajador/borrar/' + employee.numconemp">Eliminar</a>
-                    </div>
-                </div>
-            </td>
         </tr>
     </tbody>
 </table>
