a47b73114ed427bff724f128598c1b6abdfeb585
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-09-23 14:43:04 -0500

    Visualizacion de los alumnos

--- resources/js/components/TableStudents.vue
+++ resources/js/components/TableStudents.vue
@@ -1,21 +1,21 @@
 <template>
 <div class="table-responsive">
-    <table id="tableEmployees" class="table table-striped text-center border-bottom">
+    <table id="tableStudents" class="table table-striped text-center border-bottom">
         <thead>
             <tr class="thead-dark">
-                <th>N.C.</th>
+                <th>MATRÍCULA</th>
                 <th>CORREO</th>
                 <th>NOMBRE</th>
-                <th>DEPENDENCIA</th>
+                <th>CARRERA</th>
             </tr>
         </thead>
 
         <tbody>
-            <tr v-for="employee in employees" v-bind:key="">
-                <td>{{ employee.numconemp }}</td>
-                <td>{{ employee.email }}</td>
-                <td>{{ employee.nombre + ' ' + employee.apepat + ' ' + employee.apemat }}</td>
-                <td>{{ employee.dependencia }}</td>
+            <tr v-for="student in students" v-bind:key="">
+                <td>{{ student.numconemp }}</td>
+                <td>{{ student.email }}</td>
+                <td>{{ student.nombre + ' ' + student.ap_paterno + ' ' + student.ap_materno }}</td>
+                <td>{{ student.carrera }}</td>
             </tr>
         </tbody>
     </table>
@@ -29,20 +29,20 @@ import Swal from 'sweetalert2';
 export default {
     data: function () {
         return {
-            employees: []
+            students: []
         }
     },
     created() {
         Swal.showLoading();
-        this.getEmployees('empleados/todos')
+        this.getStudents('alumnos/todos')
         .then(() => Swal.close())
     },
     methods: {
-        getEmployees(url) {
+        getStudents(url) {
             return Axios.get(url)
             .then(res => {
                 console.log(res.data)
-                this.employees = res.data;
+                this.students = res.data;
             })
         }
     }
