28b8268447d5f3a09794902253734bd768d7738f
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-09-23 16:03:28 -0500

    Busqueda de empleador por numero de control

--- resources/js/components/TableEmployeesComponent.vue
+++ resources/js/components/TableEmployeesComponent.vue
@@ -1,23 +1,33 @@
 <template>
-<table id="tableEmployees" class="table table-striped text-center border-bottom">
-    <thead>
-        <tr class="thead-dark">
-            <th>N.C.</th>
-            <th>CORREO</th>
-            <th>NOMBRE</th>
-            <th>DEPENDENCIA</th>
-        </tr>
-    </thead>
 
-    <tbody>
-        <tr v-for="employee in employees" v-bind:key="">
-            <td>{{ employee.numconemp }}</td>
-            <td>{{ employee.email }}</td>
-            <td>{{ employee.nombre + ' ' + employee.apepat + ' ' + employee.apemat }}</td>
-            <td>{{ employee.dependencia }}</td>
-        </tr>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <div class="search-bar mb-2">
+        <form id="formSearch" action="#" class="form-inline" v-on:submit.prevent="buscar">
+            <input class="form-control mr-2" type="text" v-model="numero_control" placeholder="Matrícula...">
+            <button class="btn btn-primary" type="submit">Enviar</button>
+        </form>
+    </div>
+
+    <table id="tableEmployees" class="table table-striped text-center border-bottom">
+        <thead>
+            <tr class="thead-dark">
+                <th>N.C.</th>
+                <th>CORREO</th>
+                <th>NOMBRE</th>
+                <th>DEPENDENCIA</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for="employee in employees" v-bind:key="employee.numconemp">
+                <td>{{ employee.numconemp }}</td>
+                <td>{{ employee.email }}</td>
+                <td>{{ employee.nombre + ' ' + employee.apepat + ' ' + employee.apemat }}</td>
+                <td>{{ employee.dependencia }}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 </template>
 
 <script>
@@ -27,21 +37,34 @@ import Swal from 'sweetalert2';
 export default {
     data: function () {
         return {
-            employees: []
+            employees: [],
+            numero_control: ''
         }
     },
+
     created() {
         Swal.showLoading();
         this.getEmployees('empleados/todos')
         .then(() => Swal.close())
     },
+
     methods: {
-        getEmployees(url) {
-            return Axios.get(url)
+        async getEmployees(url, data = {}) {
+            return await Axios.get(url, data)
             .then(res => {
                 console.log(res.data)
                 this.employees = res.data;
             })
+        },
+
+        buscar() {
+            Swal.showLoading();
+            this.getEmployees('empleados/todos', {
+                params: {
+                    numero_control: this.numero_control
+                }
+            })
+            .then(() => Swal.close())
         }
     }
 }
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -6,30 +6,6 @@
 <div class="container">
     <h1>Empleados</h1>
 
-    <div class="table-responsive">
-        {{-- <tbody id="tbody_trabajadores">
-            @foreach($employees as $employee)
-                <tr>
-                    <td>{{ $employee->numconemp }}</td>
-                    <td>{{ $employee->nombre . ' ' . $employee->apepat }}</td>
-                    <td>{{ $employee->dependencia }}</td>
-
-                    <td class="pt-1 pb-1">
-                        <div class="dropdown" data-item="{{ $employee->numconemp }}">
-                            <button id="menuRow{{ $employee->nombre }}" class="btn btn-secondary dropdown-toggle w-100" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                settings
-                            </button>
-                            
-                            <div class="dropdown-menu shadow trabajador-actions" aria-labelledby="menuRow{{ $employee->nombre }}">
-                                <a class="dropdown-item btn-warning" type="button" data-toggle="modal" data-target="#editTrabajador">Editar</a>
-                                <a class="dropdown-item btn-danger" href="{$base_url}trabajador/borrar/{{ $employee->numconemp }}">Eliminar</a>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody> --}}
-        <table-employees-component></table-employees-component>
-    </div>
+    <table-employees-component></table-employees-component>
 </div>
 @endsection
