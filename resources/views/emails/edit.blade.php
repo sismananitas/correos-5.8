2c36a0f8277d09f623a4eb0f41a585288ed42327
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-10-14 14:46:32 -0500

    muestra de datos al editar correo

--- resources/views/emails/edit.blade.php
+++ resources/views/emails/edit.blade.php
@@ -5,15 +5,18 @@
     <div class="card col-md-6 mt-3 p-4">
         <h1>{{ $correo->login }}</h1>
         <h2>{{ $emailable->nombre . ' ' . $emailable->apepat . ' ' . $emailable->apemat }}</h2>
+        <hr>
+
+        @include('emails.show-errors')
 
         <form id="formEditar" action="{{ route('correos.update', ['correo' => $correo->id]) }}" method="POST" autocomplete="off">
             @csrf @method('PUT')
 
             <label for="login">Login</label>
-            <input id="login" class="form-control" type="email" name="login" required>
+            <input id="login" class="form-control" type="email" name="login" required value="{{ $correo->login }}">
 
             <label for="password">Contraseña</label>
-            <input id="password" class="form-control" type="text" name="password" required>
+            <input id="password" class="form-control" type="text" name="password" required value="{{ $correo->password }}">
 
             <label for="solicitante">Solicitante del cambio</label>
             <input id="solicitante" class="form-control" type="text" name="solicitante" required>
@@ -30,7 +33,7 @@
 
                 <div class="col">
                     <label for="status">Estatus</label>
-                    <select class="form-control" name="status" id="status" value="{{ old('status') }}" required>
+                    <select class="form-control" name="status" id="status" value="{{ old('status') }}" required value="{{ $correo->status }}">
                         <option value="">- Elegir -</option>
                         <option value="Activo">Activo</option>
                         <option value="Suspendido">Suspendido</option>
@@ -39,8 +42,9 @@
                 </div>
             </div> 
             
-            <div class="mt-4">
+            <div class="my-4">
                 <button class="btn btn-warning" type="submit">Actualizar</button>
+                <a class="btn btn-secondary" href="{{ route('correos.index') }}">Cancelar</a>
             </div>
         </form>
 
