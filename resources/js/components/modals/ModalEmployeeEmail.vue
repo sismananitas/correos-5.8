ac3574d882ae18fe9211571ca5ed4ff956388a2f
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-11-11 17:01:13 -0600

    update

--- resources/js/components/modals/ModalEmployeeEmail.vue
+++ resources/js/components/modals/ModalEmployeeEmail.vue
@@ -7,38 +7,38 @@
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
-                <form id="formEmployee" :action="postUrl" method="POST" autocomplete="off">
-                    <input id="editMode" type="hidden" name="_editMode" value="false">
+                <form-wizard>
+                    <tab-content title="Personal details">
+                        <form id="formEmployee" :action="postUrl" method="POST" autocomplete="off">
+                            <input id="editMode" type="hidden" name="_editMode" value="false">
 
-                    <div class="modal-body">
-                        <div class="card-body">
-                            Número de control
-                            <input list="empleados" class="form-control" type="text" name="control_number" value="" required>
+                            <div class="modal-body">
+                                <div class="card-body">
+                                    Número de control
+                                    <input list="empleados" class="form-control" type="text" name="control_number" value="" required>
 
-                            <datalist id="empleados">
-                                <option v-for="employee in employees" :key="employee.id" :value="employee.numconemp">
-                                    {{ employee.numconemp + ' - ' + employee.nombre }}
-                                </option>
-                            </datalist>
-                        </div>                      
-                    </div>
+                                    <datalist id="empleados">
+                                        <option v-for="employee in employees" :key="employee.id" :value="employee.numconemp">
+                                            {{ employee.numconemp + ' - ' + employee.nombre }}
+                                        </option>
+                                    </datalist>
+                                </div>                      
+                            </div>
+                            
+                            <div class="modal-footer">
+                                <button class="btn btn-primary" type="submit">registrar</button>
+                            </div>
+                        </form>
+                    </tab-content>
 
-                    <form-wizard>
-                        <tab-content title="Personal details">
-                            My first tab content
-                        </tab-content>
-                        <tab-content title="Additional Info">
-                            My second tab content
-                        </tab-content>
-                        <tab-content title="Last step">
-                            Yuhuuu! This seems pretty damn simple
-                        </tab-content>
-                    </form-wizard>
+                    <tab-content title="Additional Info">
+                        My second tab content
+                    </tab-content>
                     
-                    <div class="modal-footer">
-                        <button class="btn btn-primary" type="submit">registrar</button>
-                    </div>
-                </form>
+                    <tab-content title="Last step">
+                        Yuhuuu! This seems pretty damn simple
+                    </tab-content>
+                </form-wizard>
             </div>
         </div>
     </div>
