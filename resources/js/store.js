0479678cd25f58de4bf7c36d23e9f697cc2e288c
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-11-19 00:53:57 -0600

    update

--- resources/js/store.js
+++ resources/js/store.js
@@ -6,6 +6,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
+        base_url: 'http://148.218.66.73/correos/public',
         response: null,
         errors: null,
         emails: [],
@@ -48,26 +49,26 @@ export default new Vuex.Store({
 
     actions: {
         getEmails({ commit }) {
-            axios.get('/api/emails')
+            axios.get(this.base_url + '/api/emails')
             .then(res => {
                 commit('setEmails', res.data);
             })
         },
         showEmail({ commit }, id) {
-            axios.get('/api/emails/' + id)
+            axios.get(this.base_url + '/api/emails/' + id)
             .then(res => {
                 commit('setEmail', res.data)
             })
         },
 
         getStudents({ commit }) {
-            axios.get('/api/students')
+            axios.get(this.base_url + '/api/students')
             .then(res => {
                 commit('setStudents', res.data);
             })
         },
         showStudent({ commit }, id) {
-            axios.get('/api/students/' + id)
+            axios.get(this.base_url + '/api/students/' + id)
             .then(res => {
                 commit('setStudent', res.data)
             })
@@ -75,7 +76,7 @@ export default new Vuex.Store({
 
         getEmployees({ commit }) {
             Swal.showLoading()
-            axios.get('/api/employees')
+            axios.get(this.base_url + '/api/employees')
             .then(res => {
                 commit('setEmployees', res.data)
                 Swal.close()
