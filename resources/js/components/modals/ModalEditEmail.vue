1ca7fb8ab2c285bf8162f418e84c1a15e1cfeb5e
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-11-25 15:44:25 -0600

    update

--- resources/js/components/modals/ModalEditEmail.vue
+++ resources/js/components/modals/ModalEditEmail.vue
@@ -14,6 +14,7 @@
                     autocomplete="off"
                     @submit="sendForm"
                 >
+                    <input type="hidden" name="_method" value="put">
                     <!-- Modal body -->
                     <div class="modal-body">
                         <div class="form-group">
