369ce913d961ee0297a19c1ca8b1078f6591e07f
Jesus Adame <desarrollo@lasmananitas.com.mx>
2019-12-16 13:28:36 -0600

    Update

--- resources/js/components/ReportComponent.vue
+++ resources/js/components/ReportComponent.vue
@@ -104,6 +104,7 @@ export default {
             })
             .catch(error => {
                 if (error.response.status === 422) {
+                    swal.close()
                     this.errors = error.response.data.errors
                 }
             })
