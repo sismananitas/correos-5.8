a1bc0abff081af8e0cde565d6dc5840f5c3ea495
Jesus Adame <desarrollo@lasmananitas.com.mx>
2020-02-02 22:30:34 -0600

    update

--- resources/js/components/modals/ModalStudentEmail.vue
+++ resources/js/components/modals/ModalStudentEmail.vue
@@ -17,7 +17,7 @@
                             ref="formAlumno"
                             action="#"
                             autocomplete="off"
-                            @submit="preventDefault"
+                            @submit.prevent=""
                         >
                             <div class="modal-body">
                                 <div class="form-group">
