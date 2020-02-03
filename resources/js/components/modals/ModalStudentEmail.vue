6789e31a35b93da60039eac74bdf2ed27fb4297d
Jesus Adame <desarrollo@lasmananitas.com.mx>
2020-02-02 19:00:43 -0600

    update

--- resources/js/components/modals/ModalStudentEmail.vue
+++ resources/js/components/modals/ModalStudentEmail.vue
@@ -152,7 +152,7 @@ export default {
             let form = this.$refs.formAlumno
             let dataJson = new FormData(form)
 
-            await this.sendPostForm({ url: '/correo/alumnos', data: dataJson })
+            await this.sendPostForm({ url: '/students/emails', data: dataJson })
             .then(() => {
                 if (this.response) {
                     swal.close()
